a84efdf18d178792eb42590b6c90582b14c37837
Ashok Patidar <flash.ria.developer@gmail.com>
2018-02-01 15:33:59 -0500

    commit new change

--- src/index.js
+++ src/index.js
@@ -1,42 +1,42 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-// import { Provider } from "react-redux";
-// import { App } from "./App";
-// import { store } from "./_helpers";
+import { Provider } from "react-redux";
+import { App } from "./App";
+import { store } from "./_helpers";
 
-// import registerServiceWorker from "./registerServiceWorker";
-
-// // setup fake backend
-// import { configureFakeBackend } from './_helpers';
-// configureFakeBackend();
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   document.getElementById("root")
-// );
-// registerServiceWorker();
-
-import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-
-import { store } from './_helpers';
-import { App } from './App';
 import registerServiceWorker from "./registerServiceWorker";
 
 // setup fake backend
 import { configureFakeBackend } from './_helpers';
 configureFakeBackend();
 
-render(
-    <Provider store={store}>
-        <App />
-        
-    </Provider>,
-    document.getElementById('root')
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
 registerServiceWorker();
+
+// import React from 'react';
+// import { render } from 'react-dom';
+// import { Provider } from 'react-redux';
+
+// import { store } from './_helpers';
+// import { App } from './App';
+// import registerServiceWorker from "./registerServiceWorker";
+
+// // setup fake backend
+// import { configureFakeBackend } from './_helpers';
+// configureFakeBackend();
+
+// render(
+//     <Provider store={store}>
+//         <App />
+        
+//     </Provider>,
+//     document.getElementById('root')
+// );
+// registerServiceWorker();
